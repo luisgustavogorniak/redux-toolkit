acccedb3fbdbbd9e3ae057ccd10389c060b5346f
unknown <gustavogorniak@gmail.com>
2023-04-18 16:02:47 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Product management React Redux
+
+Download the files and run:
+```
+npm i
+```
+To install node_modules folder.
+
+And then:
+```
+npm start
+```
+To start the development server.
+
+You will also need a Database that connects to the process of fetching data at cart-actions.js file.
+
+## How it works?
+
+It's a app that allows the user to manage products to cart for Redux
+and its toolkit study porpuses.
---
+++ src/components/Shop/Products.js
@@ -0,0 +1,38 @@
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 6,
+    title: "Product #1",
+    description: "This is my first product",
+  },
+  {
+    id: "p2",
+    price: 5,
+    title: "Product #2",
+    description: "This is my second product",
+  },
+];
+
+const Products = (props) => {
+  return (
+    <section className={classes.products}>
+      <h2>Buy your favorite products</h2>
+      <ul>
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
